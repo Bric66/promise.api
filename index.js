d37e7af48a13310d862ea1f8034c44efde4c3b9f
Bric66 <115704191+Bric66@users.noreply.github.com>
2022-10-19 16:01:30 +0200

    ça y estgit add .git add .

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+require('dotenv').config();
+const usernameApi=process.env.USERKEY;
+const foodApi=process.env.FOODKEY
+
 const axios = require('axios');
 
-const foodList = axios.get('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
-const userApi = axios.get('https://randomuser.me/api/');
 
+const foodList = axios.get(`${foodApi}/learning-area/javascript/apis/fetching-data/can-store/products.json`);
+const userApi =  axios.get(`${usernameApi}/api/`);
 
 // todo transform this in a new async await function.
 function getUserInfosWithThen() {
-
+   userApi.then(response => {
+    console.log(response.data.results[0].name.title + ' ' + response.data.results[0].name.first + ' ' + response.data.results[0].name.last);
+        foodList.then(food => console.log(food.data.map(foodName => foodName.name)))
+   })
+}
+ 
+async function getUserInfosWithAsyncAwait() {
+    const userApi =  await axios.get(`${usernameApi}/api/`);
+    const foodList = await axios.get(`${foodApi}/learning-area/javascript/apis/fetching-data/can-store/products.json`);
+    console.log(userApi.data.results[0].name.title + ' ' + userApi.data.results[0].name.first + ' ' + userApi.data.results[0].name.last);
+    console.log(foodList.data.map(foodName => foodName.name))
 }
 
 
-function getUserInfosWithAsyncAwait() {
+(async() => {getUserInfosWithThen()}) ();
 
-}
-
-getUserInfosWithThen();
+(async() => {getUserInfosWithAsyncAwait()}) ();
